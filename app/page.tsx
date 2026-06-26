8196751a0ed4d143cd0fd8c1d453f919ad7ef58f
warehouse-hue <warehouse@mgbbackline.com>
2026-06-26 12:26:16 +1000

    test change

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>MGB Inventory App 🚀</h1>
-      <p>If you see this, deployment is working.</p>
-    </main>
-  );
+  return <h1>HELLO MGB INVENTORY 🚀</h1>;
 }
